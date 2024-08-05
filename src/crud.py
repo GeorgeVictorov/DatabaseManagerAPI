cc5a0623d568aaeb49b1e86086589697a0c984c6
George <georgiiviktorov@MacBook-Pro.local>
2024-08-05 12:48:26 +0400

    exception handling crud

--- src/crud.py
+++ src/crud.py
@@ -1,29 +1,41 @@
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 from typing import cast
 from .models import ParserConfig
 
 
-def create_resource(db: Session, resource: ParserConfig):
-    existing_resource = db.query(ParserConfig).filter(
-        (cast("ColumnElement[bool]", ParserConfig.name == resource.name) | (ParserConfig.url == resource.url))
-    ).first()
-    if existing_resource:
-        raise ValueError("Resource with the same name or url already exists.")
-
-    db.add(resource)
-    db.commit()
-    db.refresh(resource)
-    return resource
-
-
 def get_resources(db: Session):
-    return db.query(ParserConfig).order_by(ParserConfig.resource_id.asc()).all()
+    try:
+        return db.query(ParserConfig).order_by(ParserConfig.resource_id.asc()).all()
+    except SQLAlchemyError:
+        raise RuntimeError(f"A database error occurred.")
+
+
+def create_resource(db: Session, resource: ParserConfig):
+    try:
+        existing_resource = db.query(ParserConfig).filter(
+            (cast("ColumnElement[bool]", ParserConfig.name == resource.name) | (ParserConfig.url == resource.url))
+        ).first()
+        if existing_resource:
+            raise ValueError("Resource with the same name or url already exists.")
+
+        db.add(resource)
+        db.commit()
+        db.refresh(resource)
+        return resource
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise RuntimeError(f"A database error occurred.")
 
 
 def delete_resource(db: Session, resource_id: int):
-    resource = db.query(ParserConfig).filter(
-        cast("ColumnElement[bool]", ParserConfig.resource_id == resource_id)).first()
-    if resource:
-        db.delete(resource)
-    db.commit()
-    return resource
+    try:
+        resource = db.query(ParserConfig).filter(
+            cast("ColumnElement[bool]", ParserConfig.resource_id == resource_id)).first()
+        if resource:
+            db.delete(resource)
+        db.commit()
+        return resource
+    except SQLAlchemyError:
+        db.rollback()
+        raise RuntimeError(f"A database error occurred.")
